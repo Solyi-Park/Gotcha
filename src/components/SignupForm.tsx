5522f32422f503f6ce88cbebfb68e36b5f78f63c
Solyi Park <itsoholy21@gmail.com>
2024-07-26 02:59:52 +0900

    회원가입 유효성검사 추가

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,25 +1,38 @@
 "use client";
+import { useErrorMessage } from "@/hooks/errorMessage";
+import { validate } from "@/utils/validate";
 
 import { useRouter } from "next/navigation";
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
 
 type Props = {
   csrfToken: string;
 };
+
 export default function SignupForm({ csrfToken }: Props) {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passRef = useRef<HTMLInputElement>(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const router = useRouter();
+
+  const { nameError, emailError, passwordError, confirmPasswordError } =
+    useErrorMessage({ name, email, password, confirmPassword });
+
+  const isButtonDisabled =
+    nameError !== null ||
+    emailError !== null ||
+    passwordError !== null ||
+    confirmPasswordError !== null;
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
     const formData = new FormData();
-    formData.append("name", nameRef.current?.value ?? "");
-    formData.append("email", emailRef.current?.value ?? "");
-    formData.append("password", passRef.current?.value ?? "");
+    formData.append("name", name ?? "");
+    formData.append("email", email ?? "");
+    formData.append("password", password ?? "");
 
     fetch("/api/auth/signup", {
       method: "POST",
@@ -35,24 +48,65 @@ export default function SignupForm({ csrfToken }: Props) {
   return (
     <form method="post" className="flex flex-col" onSubmit={handleSubmit}>
       <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-      <label>
-        이름
-        <input name="name" ref={nameRef} type="text" className="border" />
-      </label>
-      <label>
-        이메일
-        <input name="email" ref={emailRef} type="email" className="border" />
-      </label>
-      <label>
-        비밀번호
-        <input
-          name="password"
-          ref={passRef}
-          type="password"
-          className="border"
-        />
-      </label>
-      <button className="border" type="submit">
+      <div>
+        <label>
+          이름
+          <input
+            name="name"
+            value={name}
+            type="text"
+            className="border"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <p>{name}</p>
+        <p>{nameError}</p>
+      </div>
+      <div>
+        <label>
+          이메일
+          <input
+            name="email"
+            value={email}
+            type="email"
+            className="border"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <p>{emailError}</p>
+      </div>
+      <div>
+        <label>
+          비밀번호
+          <input
+            name="password"
+            value={password}
+            type="password"
+            className="border"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {passwordError}
+        </label>
+        <p></p>
+      </div>
+      <div>
+        <label>
+          비밀번호 확인
+          <input
+            name="passwordCheck"
+            value={confirmPassword}
+            type="password"
+            className="border"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+        <p>{confirmPasswordError}</p>
+      </div>
+      <button
+        className={`border  ${isButtonDisabled && "bg-gray-200 text-white"}`}
+        type="submit"
+        disabled={isButtonDisabled}
+      >
         회원가입
       </button>
     </form>
---
+++ src/hooks/errorMessage.ts
@@ -0,0 +1,45 @@
+import { validate } from "@/utils/validate";
+import { useEffect, useState } from "react";
+
+type Props = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+export function useErrorMessage({
+  name,
+  email,
+  password,
+  confirmPassword,
+}: Props) {
+  const [nameError, setNameError] = useState<string | null>(null);
+  const [emailError, setEmailError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [confirmPasswordError, setConfirmPasswordError] = useState<
+    string | null
+  >(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setNameError(name ? validate.name(name) : null);
+      setEmailError(email ? validate.email(email) : null);
+      setPasswordError(password ? validate.password(password) : null);
+      setConfirmPasswordError(
+        confirmPassword
+          ? validate.confirmPassword(password, confirmPassword)
+          : null
+      );
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [name, email, password, confirmPassword]);
+
+  return {
+    nameError,
+    emailError,
+    passwordError,
+    confirmPasswordError,
+  };
+}
